9bc5874daf3078e4983f06465d54eab5a6595d4c
Marcin Stecki <madsheeppl@gmail.com>
2012-05-10 13:05:55 +0200

    transport!

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'hipchat'
+require 'pry'
+require 'json'
+
+get "/" do
+  "<img style='height: 100%' src='http://blog.stackoverflow.com/wp-content/uploads/error-lolcat-problemz.jpg'>"
+end
+
+post "/" do
+  token = params[:auth_token]
+  room_id = params[:room_id]
+  hoptoad_data = JSON.parse(request.body.read)
+  respond_body = "#{hoptoad_data["error"]["error_message"]} <br /> <a href='http://airbrake.io/projects/#{hoptoad_data["error"]["id"]}/errors'>View on Airbake...</a>"
+  hipchat = HipChat::Client.new(token)
+  hipchat[room_id].send("Airbake", respond_body, :color => :red)
+  "Ok"
+end
