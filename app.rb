f98a2b465b08038b7758dfc028e439db3f28e49d
Marcin Stecki <madsheeppl@gmail.com>
2012-05-10 13:16:19 +0200

    better output

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ post "/" do
   token = params[:auth_token]
   room_id = params[:room_id]
   hoptoad_data = JSON.parse(request.body.read)
-  respond_body = "#{hoptoad_data["error"]["error_message"]}<br />#{hoptoad_data["error"]["error_class"]}<br /><a href='http://airbrake.io/projects/#{hoptoad_data["error"]["id"]}/errors'>View on Airbake...</a>"
+  respond_body = "#{hoptoad_data["error"]["error_class"]}:#{hoptoad_data["error"]["error_message"]}<br /><a href='http://airbrake.io/projects/#{hoptoad_data["error"]["id"]}/errors'>View on Airbake...</a>"
   hipchat = HipChat::Client.new(token)
   hipchat[room_id].send("Airbake", respond_body, :color => :red)
   "Ok"
